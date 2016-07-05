60f5790b121d7180c8621234af8808a49097d432
AntonPavl <pavlenok.anton@gmail.com>
2016-07-05 12:04:56 +0300

    Create

---
+++ BitMath/BitMath.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+namespace BitMath
+{
+    public class BitAlghoritm
+    {
+        /// <summary>
+        /// Get a sign
+        /// </summary>
+        /// <param name="a">number</param>
+        /// <returns>0 if positive, 1 if negative</returns>
+        public static int ComputeSign(int a)
+        {
+            var x = Int32.MaxValue;
+            var b = Int32.MinValue;
+            var t = -2;
+            return (a>>31) & 1;
+        }
+        /// <summary>
+        /// Compute a and b;
+        /// </summary>
+        /// <param name="a">First number</param>
+        /// <param name="b">SeCond nubmer</param>
+        /// <returns>1 if equals, a if a>b , b if b>a </returns>
+        public static int ComputeValue(int a,int b)
+        {
+            int i = 31;
+            while(i!=0)
+            {
+                if ( (a & (1 << i)) > (b & (1 << i)) )
+                {
+                    return a;
+                }
+                if ((a & (1 << i)) < (b & (1 << i)))
+                {
+                    return b;
+                }
+                i--;
+            }
+            return 1;
+        }
+    }
+}
---
+++ BitOperations/BitMathTests/BitMathTest.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using BitMath;
+
+
+namespace BitMathTests
+{
+    [TestClass]
+    public class BitMathTest
+    {
+
+        [TestMethod]
+        public void ComputeSign_100_0()
+        {
+            int a = 100;
+            double expectedResult = 0;
+            double realResult = BitAlghoritm.ComputeSign(a);
+            Assert.AreEqual(expectedResult, realResult);
+        }
+        [TestMethod]
+        public void ComputeSign_Negative100_1()
+        {
+            int a = -100;
+            double expectedResult = 1;
+            double realResult = BitAlghoritm.ComputeSign(a);
+            Assert.AreEqual(expectedResult, realResult);
+        }
+        [TestMethod]
+        public void ComputeValue_10andNegative10_1()
+        {
+            int a = 10;
+            int b = -10;
+            double expectedResult = a;
+            double realResult = BitAlghoritm.ComputeValue(a, b);
+            Assert.AreEqual(expectedResult, realResult);
+        }
+        [TestMethod]
+        public void ComputeValue_Negative10andNegative10_negative1()
+        {
+            int a = -10;
+            int b = -10;
+            double expectedResult = 1;
+            double realResult = BitAlghoritm.ComputeValue(a, b);
+            Assert.AreEqual(expectedResult, realResult);
+        }
+        [TestMethod]
+        public void ComputeValue_1626436andNegative14125363_0()
+        {
+            int a = 1626436;
+            int b = 14125363;
+            double expectedResult = b;
+            double realResult = BitAlghoritm.ComputeValue(a, b);
+            Assert.AreEqual(expectedResult, realResult);
+        }
+    }
+}
